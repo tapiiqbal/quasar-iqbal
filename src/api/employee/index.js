47f7dda1eebbc40ee85fc6002d83c3abe0afb164
tapiiqbal <tapiiqbal@gmail.com>
2019-07-30 14:25:53 +0700

    quasar

---
+++ src/api/employee/index.js
@@ -0,0 +1,17 @@
+import VueResource from 'vue-resource'
+import Vue from 'vue'
+import {getApiNoAuth} from './../utils'
+
+Vue.use(VueResource)
+
+export default {
+
+    getEmployee(window) {
+        return getApiNoAuth().get('Employees')
+        .then(function (response){
+            return response.data
+        }).catch(function(err){
+            console.log(err)
+        })
+    },
+}
---
+++ src/components/Employee.vue
@@ -0,0 +1,102 @@
+<template>
+    <q-page class="flex flex-center">
+    <p>{{employeeData}}</p>
+
+    <div class="row">
+
+        <div class="col-sm-6">
+            <p>ini first name</p>
+            <button class="danger" @click="getFirstNameOnly()">First Name</button>
+            <div>
+                <p>{{firstNameDataOnly}}</p>
+            </div>
+        </div>
+
+        <div class="col-sm-6">
+            <p> Ini lastname</p>
+            <button class="danger" @click="getLastNameOnly()">Last Name</button>
+            <div>
+                <p>{{lastNameDataOnly}}</p>
+            </div>
+        </div>
+    </div>
+    </q-page>
+</template>
+
+<style>
+
+</style>
+
+<script>
+import Vue from 'vue';
+import employee from "../api/employee/index";
+import VMDateRangePicker from "v-md-date-range-picker";
+
+Vue.use(VMDateRangePicker);
+export default {
+    name: 'Employee' ,
+
+    data () {
+        return {
+            employeeData: {},
+            totalEmployee; 0,
+            nameEmployee: 'sapi',
+            firstDataOnly: [],
+            lastNameDataOnly: []
+        }
+    },
+
+    methods: {
+        getFirstNameOnly() {
+            con = self = this;
+            employee.getEmployee(windows).then(function (datas) {
+                console.log('ini data nama depan', datas)
+                for (let i=0; i < datas.length; i++){
+                    self.firstDataOnly.push(datas[i].firstname)
+                }
+                return datas;
+            }).catch(function (err){
+                console.log(err)
+            })
+        },
+
+        getLastNameOnly() {
+            con = self = this;
+            employee.getEmployee(windows).then(function (datas) {
+                console.log('ini data nama depan', datas)
+                for (let i=0; i < datas.length; i++){
+                    self.firstDataOnly.push(datas[i].lastname)
+                }
+                return datas;
+            }).catch(function (err){
+                console.log(err)
+            })
+        },
+    },
+    
+    beforeCreate(){
+        let self = this
+
+        employee.getEmployee(window).then(function (datas){
+            return datas
+        }).then(function (res){
+            console.log(res)
+            self.employeeData = res
+        }).catch(function(err){
+            console.log(err)
+        })
+
+        console.log()
+    },
+
+    created(){
+
+    },
+
+    berforeMount(){
+
+    },
+}
+</script>
+
+
